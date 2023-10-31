ad67c0853a618627cd574215827e3b6eb29c9cbf
Igor <igor.balandin@gmx.de>
2023-10-31 10:36:56 +0100

    OOP

---
+++ Javatopia/src/com/bal/oop/gettingstarted/BenefitsOfOOP.java
@@ -0,0 +1,23 @@
+package com.bal.oop.gettingstarted;
+
+public class BenefitsOfOOP {
+    /*
+    OOP is about objects. These objects interact with each other to perform various tasks. As analogy think about a
+    car. Car consists of hundreds or thousands of collaborating objects. Almost all these objects are reusable and
+    replaceable. So if any of them malfunction, a technician can put it out and replace it with another object.
+    Applications built using object oriented programming techniques are exactly the same.
+
+    Benefits:
+    > Reduced Complexity
+        > breaking down a large complex application into smaller, more manageable and easier to maintain objects
+    > Easier Maintenance
+        > if an object is not working properly, we can only focus on the code of that object, not the entire application
+          we can either fix that object or plug in a different object as a replacement
+    > Code Reuse
+        > Once we build and test an object, we can use it in other application.
+          For example, let's say we are building an image editing application. In this application we're going to give
+          our users various filters  to use their photos. Once we build these filters, then we can reuse them in other
+          image editing applications. And this means we are going to spend less time to building applications.
+     > Faster Development
+     */
+}
---
+++ Javatopia/src/com/bal/oop/gettingstarted/ProgrammingParadigms.java
@@ -0,0 +1,49 @@
+package com.bal.oop.gettingstarted;
+
+public class ProgrammingParadigms {
+    /*Programming paradigms/styles of coding
+    > procedural
+    > functional
+    > oop
+    > event-driven
+    > logic
+    > Aspect-oriented
+
+    Example:
+    > SmallTalk programming language (OOP)
+        > support a single paradigm
+    > Python, Ruby, Java, Javascript ...
+        > support multiple paradigms
+
+    In OOP, everything is based on the concept of objects. These objects are units that contain some
+    Data (State) and operations on the data also called method (behavior). These are the methods or functions that
+    modify the data. In OOP, you bring together, data and methods (behavior) that operate on a single object.
+
+    In FUNCTIONAL PROGRAMMING, we take the opposite approach, we assume that data and behavior are fundamentally
+    different, so we keep them separate. This is completely different style of programming
+
+    You can solve the same problem using both these paradigms, your code will end up looking different.
+
+    OOP
+    > is difficult to understand/learn
+    > is Complex
+    > most books and courses out there fail to explain it properly
+
+    - it depends on the problem  you're trying to solve. One of the major issues we have in the software industry
+    these days is that developers are more excited about languages, tools and frameworks than solving problems
+
+    Problem Solving
+    Process of DEFINING A PROBLEM, identifying and comparing DIFFERENT SOLUTIONS, and picking the one that the best
+    solves that problem to the CONTEXT and CONSTRAINTS.
+    die das Problem je nach Kontext und Einschränkungen am besten löst.
+    A lot of developers out there dismiss the context and constraints, and they see things as black or white.
+    They have their favourite languages and tools, and they use them for every single problem
+
+    Which style is better?
+    depends on the:
+    - problem
+    - context
+    - budget
+    don't fall in love with one solution and apply it to every single problem
+     */
+}
