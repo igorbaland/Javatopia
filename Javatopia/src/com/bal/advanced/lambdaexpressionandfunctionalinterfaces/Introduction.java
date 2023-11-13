b3e3e047917d150162a304725f12c86b7b9e8d57
Igor <igor.balandin@gmx.de>
2023-11-13 14:11:24 +0100

    Introduction functional programming and lambda expression.

---
+++ Javatopia/src/com/bal/advanced/lambdaexpressionandfunctionalinterfaces/Introduction.java
@@ -0,0 +1,23 @@
+package com.bal.advanced.lambdaexpressionandfunctionalinterfaces;
+
+public class Introduction {
+    /*
+    > Functional programming
+         > Its one of the many programming paradigms or styles of programming that makes implementing certain tasks
+         really easy and robust.
+
+    Lambda expression
+        > were introduced in Java 8 in 2014, to allow us to use functional programming techniques in Java.
+
+    In this section we are going to talk about
+        > lambda expressions
+        > functional interfaces
+        > consumers
+        > suppliers
+        > functions
+        > predicates
+
+        These building blocks are extremely important. They are used a lot in real world applications. And we are going
+        to continue using them throughout the rest of this course. Make sure to master them before going further.
+     */
+}
